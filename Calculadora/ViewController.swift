82f7e4f4db363f1930fd6a1a25338ab37d52bffb
Alumno <cesarrgomez@hotmail.com>
2022-08-18 09:54:38 -0700

    Primer commit

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lblTotalCuenta: UITextField!
+    
+    @IBAction func lblCantidadPropina(_ sender: Any) {
+        lblPorcentaje.text = "\(Int(lblCantidadPropina.value))%"
+        let Totalcuenta = Float(lblTotalCuenta.text!)
+        
+        
+    }
+    
+    @IBOutlet weak var lblCantidadPropina: UISlider!
+    
+    @IBOutlet weak var lblPorcentaje: UILabel!
+    
+    @IBOutlet weak var lblPropina: UILabel!
+    
+    
+    @IBOutlet weak var lblTotalTotal: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
 
 }
 
